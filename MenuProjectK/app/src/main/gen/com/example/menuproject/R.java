db8e4ae191e6dab59c3b78417c7cdba4f721fbf5
DennAC99 <78169473+DennAC99@users.noreply.github.com>
2021-06-22 00:35:42 -0400

    Delete MenuProjectK directory

--- MenuProjectK/app/src/main/gen/com/example/menuproject/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.example.menuproject;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
